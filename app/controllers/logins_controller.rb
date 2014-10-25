dd21b5ad6deb8f0fc8cd17c75ce310ea681a3ac7
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-10-24 22:55:55 -0400

    fixed syntax error in logins controller

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -5,7 +5,7 @@ class LoginsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      session[:current_user_id] = @user.__id
+      session[:current_user_id] = @user.id
       redirect_to root_path, notice: "Successfully logged in."
     else
       redirect_to logins_new_path, notice: "User name and/or password is incorrect."
