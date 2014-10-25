3cabddc69620b0ca8c31c2e203f1fd2b1d8dd43a
David Perry <curiousepic@gmail.com>
2014-10-24 20:45:16 -0400

    Elaborated notes controller tests

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,9 +1,32 @@
 require 'test_helper'
 
 class NotesControllerTest < ActionController::TestCase
+
   context "request POST :create" do
     setup { post :create }
     should respond_with(:ok)
-  end
 
+    context "with invalid info" do
+      setup { post :create, { user_id: users(:one).id, text: "" } }
+      should render_template('/problems/index')
+
+      # should "instantiate an invalid note" do
+      #   assert assigns[:note].invalid?
+      # end
+
+      should "not save a note" do
+      refute assigns[:note]
+      end
+    end
+
+    context "with valid info" do
+      setup { post :create, { user_id: users(:one).id, text: "asdf" } }
+      should render_template('problems/index')
+
+      should "save a note" do
+        assert assigns[:note]
+        assert assigns[:note].persisted?
+      end
+    end
+  end
 end
--- test/controllers/problems_controller_test.rb
+++ test/controllers/problems_controller_test.rb
@@ -4,21 +4,26 @@ class ProblemsControllerTest < ActionController::TestCase
   context "request GET :new" do
     setup { get :new }
     should respond_with(:ok)
+    should render_template('new')
   end
 
   context "request POST :create" do
     setup { post :create }
     should respond_with(:ok)
+
+    # assert_redirected_to problem_path(assigns(:problem))
   end
 
   context "request GET :index" do
     setup { get :index }
     should respond_with(:ok)
+    should render_template('index')
   end
 
   context "request GET :show" do
-    setup { get :show }
+    setup { get :show, { id: problems(:one) } }
     should respond_with(:ok)
+    should render_template('show')
   end
 
 end
